0645109bf0ca7e80875453d06dfe9efa30f54ed4
a14759226-glitch <a14759226@gmail.com>
2025-11-04 12:46:39 +0300

    Add files via upload

---
+++ c++_first_task_5.cpp
@@ -0,0 +1,28 @@
+﻿// first_task_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int min_n, count = 0;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        if (i == 0 || x < min_n) {
+            min_n = x;
+            count = 1;
+        }
+        else if (x == min_n) {
+            count++;
+        }
+    }
+
+    cout << count;
+}
