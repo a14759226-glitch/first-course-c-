03acb18253cd328df7938229af3a7e8285864aca
a14759226-glitch <a14759226@gmail.com>
2025-11-04 12:53:58 +0300

    Add files via upload

---
+++ c++_second_task_6.cpp
@@ -0,0 +1,29 @@
+﻿// second_task_6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    int fib = 0, curr = 1;
+    int position = 1;
+
+    while (curr < a) {
+        int next = fib + curr;
+        fib = curr;
+        curr = next;
+        position++;
+    }
+
+    if (curr == a) {
+        cout << position;
+    }
+    else {
+        cout << -1;
+    }
+
+    return 0;
+}
