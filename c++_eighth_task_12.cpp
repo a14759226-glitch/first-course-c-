4f81b6713a1be7d791f4b753fec9cf5616404dd1
a14759226-glitch <a14759226@gmail.com>
2025-11-04 13:40:51 +0300

    Add files via upload

---
+++ c++_eighth_task_12.cpp
@@ -0,0 +1,20 @@
+﻿// eighth_task_12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i<n; i++) {
+        int num;
+        cin >> num;
+
+        if (num >= 10 && num < 100 && num % 8 == 0) {
+            sum += num;
+        }
+    }
+    cout << sum;
+}
---
+++ c++_seventh_task_11.cpp
@@ -0,0 +1,22 @@
+﻿// seventh_task_11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int count =0;
+
+    for (int i = 0; i<n; i++) {
+        int num;
+        cin >> num;
+
+        if (num > 0) {
+            count++;
+        }
+    }
+    cout << count;
+}
