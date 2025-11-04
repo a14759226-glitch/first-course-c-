77e6fea8cc4b47c16a08ccf9ec17cac63d62d747
a14759226-glitch <a14759226@gmail.com>
2025-11-04 13:24:44 +0300

    Add files via upload

---
+++ c++_fifth_task_9.cpp
@@ -0,0 +1,17 @@
+﻿// fifth_task_9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int p = 1;
+
+    while (p <= n) {
+        cout << p << " ";
+        p = p * 2;
+    }
+}
---
+++ c++_fourth_task_8.cpp
@@ -0,0 +1,18 @@
+﻿// fourth_task_8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string dna;
+    cin >> dna;
+
+    for (int i = 0; i< dna.length(); i++) {
+        if (dna[i] =='A') cout << 'T';
+        else if (dna[i] =='T') cout << 'A';
+        else if (dna[i] =='C') cout << 'G';
+        else if (dna[i] =='G') cout << 'C';
+    }
+}
