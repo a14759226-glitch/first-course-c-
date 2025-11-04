63392585dceb3ba7b8a1d7e3a5cdaf1305582009
a14759226-glitch <a14759226@gmail.com>
2025-11-04 13:02:43 +0300

    Add files via upload

---
+++ c++_third_task_7.cpp
@@ -0,0 +1,20 @@
+﻿// third_task_7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int i=b; i>= a; i--) {
+        if (i % 7 == 0) {
+            cout << i;
+            return 0;
+        }
+    }
+
+    cout << -1;
+}
